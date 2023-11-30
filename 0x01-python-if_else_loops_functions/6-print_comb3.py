77b634f1d5d8460b02963762f1f5776a8bc4c58f
NessmaKandil <nessmakandil137@gmail.com>
2023-11-30 04:48:56 +0200

    first commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,4 @@
 for x in range(10):
     for y in range(10):
         if (x != y) and (y > x):
-            print("{:02d}".format(y + 10 * x), end='\n'\
-                  if (x == 8 and y == 9) else ", ")
+            print("{:02d}".format(y + 10 * x), end='\n' if (x == 8 and y == 9) else ", ")
