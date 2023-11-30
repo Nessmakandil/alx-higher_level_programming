8ac7568ca973a2bf31a02bb71658476b23a565ff
NessmaKandil <nessmakandil137@gmail.com>
2023-11-30 18:48:56 +0200

    first commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) - 1 == 0:
+    if len(argv) - 1 == 0:
         print("0 arguments.")
-    elif len(sys.argv) - 1 == 1:
+    elif len(argv) - 1 == 1:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
+        print("1: {}".format(argv[1]))
     else:
-        print("{} argument:".format(len(sys.argv) - 1))
-        for i, x in enumerate(sys.argv[1:]):
+        print("{} arguments:".format(len(argv) - 1))
+        for i, x in enumerate(argv[1:]):
             print("{}: {}".format(i + 1, x))
