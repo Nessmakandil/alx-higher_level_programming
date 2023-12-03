a5f4f4116781318d180f85caee96a7ced3c4b612
NessmaKandil <nessmakandil137@gmail.com>
2023-12-04 01:28:00 +0200

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0])
+    if sentence:
+        return (len(sentence), sentence[0])
+    else:
+        return (len(sentence), None)
