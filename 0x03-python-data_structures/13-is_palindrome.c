d0e55ef73f963c53a214c9a0af0b911833575ff1
NessmaKandil <nessmakandil137@gmail.com>
2023-12-04 03:59:44 +0200

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a list is palindrome
+ * @head: input list
+ * Return: 0 or 1
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int count = 0, i = 0, j, *num;
+	listint_t *list = *head;
+
+	while (list->next)
+	{
+		count++;
+		list = list->next;
+	}
+	num = malloc(sizeof(int) * count);
+	list = *head;
+
+	while (i < count)
+	{
+		num[i] = list->n;
+		list = list->next;
+		i++;
+	}
+
+	j = count - 1;
+	for (i = 0; i < round(count / 2); i++)
+	{
+		if (num[i] == num[j])
+		{
+			j--;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
