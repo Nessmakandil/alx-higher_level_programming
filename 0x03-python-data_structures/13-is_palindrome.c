9126b95f9eb51abeeae7aa7433b6d444369ff157
NessmaKandil <nessmakandil137@gmail.com>
2023-12-04 04:05:10 +0200

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -16,6 +16,10 @@ int is_palindrome(listint_t **head)
 		count++;
 		list = list->next;
 	}
+	if (count % 2 != 0)
+	{
+		return (0);
+	}
 	num = malloc(sizeof(int) * count);
 	list = *head;
 
@@ -27,7 +31,7 @@ int is_palindrome(listint_t **head)
 	}
 
 	j = count - 1;
-	for (i = 0; i < round(count / 2); i++)
+	for (i = 0; i < count / 2; i++)
 	{
 		if (num[i] == num[j])
 		{
