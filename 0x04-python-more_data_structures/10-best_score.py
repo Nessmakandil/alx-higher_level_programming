d956a9869939a5dbab4e53cfe5629b807fe10ef5
NessmaKandil <nessmakandil137@gmail.com>
2023-12-09 01:32:19 +0200

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,9 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return (None)
+    for i in a_dictionary.values():
+        if i == None:
+            return (None)
     sorted_values = sorted(a_dictionary.values())
     val = list(a_dictionary.values())
     keys = list(a_dictionary.keys())
