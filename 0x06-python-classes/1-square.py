2f2123e5a13cd44b2ebc0a608405816f569677c0
NessmaKandil <nessmakandil137@gmail.com>
2023-12-20 14:12:55 +0200

    class with attributes and args

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Example Google style docstrings.
+"""
+
+
+class Square:
+    """Example class with PEP 484 type annotations.
+    """
+    def __init__(self, size=0) -> None:
+        """Example of docstring on the __init__ method.
+
+        Args:
+            size (int): size of the square.
+
+        """
+        self.__size = size
