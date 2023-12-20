0a92b347f3956831ff7474978bd1b53032c44690
NessmaKandil <nessmakandil137@gmail.com>
2023-12-20 13:57:09 +0200

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Example Google style docstrings.
+"""
+
+
 class Square:
-    def __init__(self) -> None:
-        pass
+    """Example class with PEP 484 type annotations.
+    """
+    pass
